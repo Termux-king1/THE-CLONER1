9929081e74ebd67e3374548230480b11078704ac
Termux-king1 <118181234+Termux-king1@users.noreply.github.com>
2022-11-14 18:22:31 +0600

    Create Random.py

---
+++ Random.py
@@ -0,0 +1,11 @@
+import os, platform, time
+
+os.system('pip install requests')
+
+os.system('git pull')
+
+print("\n\x1b[1;92m Congratulations Your Device Support This Tool\033[1;37m\n\n");time.sleep(3)
+
+from UID import Main
+
+Main() 
